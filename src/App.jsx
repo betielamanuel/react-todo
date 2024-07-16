eb6c5fc5d93b7ce3495851d9987d4e7be3d2a111
John Doe <betiaman@example.com>
2024-07-15 22:40:24 -0500

    from lesson_1_2: imports AddTodoForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ToDoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
+      <AddTodoForm />
       <ToDoList />
     </div>
   );
