c5f7c8fbb84cc0a506a89097a5950595dbb0aa01
John Doe <betiaman@example.com>
2024-07-15 21:47:19 -0500

    review for lesson_1_1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-// /src/App.jsx
 // import React from "react";
-// /src/App.jsx
 
-// import React from 'react';
-import AddTodoForm from "./AddTodoForm";
+// above the App() function, create an empty Array and store it in a variable
+// names todoList
 
-function App() {
+let todoList = [
+  { id: 1, title: "Complete assignment" },
+  { id: 2, title: "Read a book" },
+  { id: 3, title: "Go for a walk" },
+];
+
+const App = () => {
   return (
     <div>
+      {/* create a level-one heading thats says "Todo List" */}
       <h1>Todo List</h1>
-      <AddTodoForm />
+      {/* create an unordered list (<ul>) */}
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
