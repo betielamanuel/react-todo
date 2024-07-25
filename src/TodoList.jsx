50ce92e8685dac8ae3df0e8ec6c8b8cdc5b79f21
John Doe <betiaman@example.com>
2024-07-24 22:43:40 -0500

    move List Item into New Todo List Item component

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,18 +1,18 @@
-// create a functional React component
-// declare a function names TodoList
-// export TodoList function as default module
+import TodoListItem from "./TodoListItem"; // Import TodoListItem
 
 const todoList = [
   { id: 1, title: "Learn React" },
-  { id: 2, title: "Clime Mt. Everest" },
+  { id: 2, title: "Climb Mt. Everest" },
   { id: 3, title: "Run a marathon" },
   { id: 4, title: "Feed the cat" },
 ];
+
 const ToDoList = () => {
   return (
     <ul>
       {todoList.map((todo) => (
-        <li key={todo.id}>{todo.title}</li>
+        // Use the TodoListItem component and pass key and todo as props
+        <TodoListItem key={todo.id} todo={todo} />
       ))}
     </ul>
   );
