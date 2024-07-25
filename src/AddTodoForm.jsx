9d9bb0a24e930e8593d32cf7b4bfa788f5846f6a
John Doe <betiaman@example.com>
2024-07-25 00:19:14 -0500

    store new todo in react state

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,6 +1,8 @@
-function AddTodoForm() {
+import PropTypes from "prop-types";
+
+function AddTodoForm(props) {
   // create a new function called handleAddTodo
-  const handleAddTodo = () => {
+  const handleAddTodo = (event) => {
     // prevent the default behavior of the form submit
     event.preventDefault();
 
@@ -11,9 +13,13 @@ function AddTodoForm() {
     // log the value of todoTitle in the console
     console.log(todoTitle);
 
+    // invoke the onAddTodo callback prop and pass todoTitle as an argument
+    props.onAddTodo(todoTitle);
+
     // reset the form so the text input value is cleared
     event.target.reset();
   };
+
   return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Title</label>
@@ -22,5 +28,10 @@ function AddTodoForm() {
     </form>
   );
 }
+
+// Prop validation for AddTodoForm
+AddTodoForm.propTypes = {
+  onAddTodo: PropTypes.func.isRequired,
+};
 
 export default AddTodoForm;
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react"; // import useState hook
+
 import ToDoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
 function App() {
+  // create a new state variable named newTodo with update
+  // function named setNewTodo
+  const [newTodo, setNewTodo] = useState("");
+
   return (
     <div>
       <h1>Todo List</h1>
-      <AddTodoForm />
+      {/*  pass setNewTodo as a callback handler prop named onAddTodo to the AddTodoForm component  */}
+
+      <AddTodoForm onAddTodo={setNewTodo} />
+      {/* Add a paragraph element that displays the value of newTodo variable */}
+      <p>New Todo: {newTodo}</p>
       <ToDoList />
     </div>
   );
