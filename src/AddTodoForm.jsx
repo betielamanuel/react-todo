6c3901a9d0ff67710da17f6049eb3b267d447d34
John Doe <betiaman@example.com>
2024-07-24 23:07:55 -0500

    handle add todo form submit

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,8 +1,23 @@
 function AddTodoForm() {
+  // create a new function called handleAddTodo
+  const handleAddTodo = () => {
+    // prevent the default behavior of the form submit
+    event.preventDefault();
+
+    // retrieve the value of the title element from
+    // the event target
+    const todoTitle = event.target.elements.title.value;
+
+    // log the value of todoTitle in the console
+    console.log(todoTitle);
+
+    // reset the form so the text input value is cleared
+    event.target.reset();
+  };
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Title</label>
-      <input type="text" id="todoTitle" />
+      <input type="text" id="todoTitle" name="title" />
       <button type="submit">Add</button>
     </form>
   );
